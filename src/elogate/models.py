12b91ad07a92838b7c26f2ea9ec62d62309a1bdd
Jasper Chan <jasperchan515@gmail.com>
2025-09-22 12:07:39 -0700

    db: add column for game ranking model
    * db: use variable to set default char field lengths
      * db: reset migrations, aerich sqlite doesn't support modifying existing
        columns

--- src/elogate/models.py
+++ src/elogate/models.py
@@ -1,17 +1,52 @@
-from enum import unique
+from enum import Enum
+
+from openskill.models import MODELS as RANKING_MODELS
+from tortoise.exceptions import ConfigurationError
 from tortoise.models import Model
-from tortoise import fields
+from tortoise.fields import CharField, IntField
+
+RankingModels = Enum("RankingModels", {cls.__name__: cls for cls in RANKING_MODELS})
+CHAR_FIELD_LEN_NAMES = 256
+
+
+class EnumField(CharField):
+    """
+    An example extension to CharField that serializes Enums
+    to and from a str representation in the DB.
+    """
+
+    def __init__(self, enum_type: type[Enum], max_length: int | None = None, **kwargs):
+        if max_length is None:
+            max_length = max([len(v.name) for v in enum_type])
+        super().__init__(max_length=max_length, **kwargs)
+        if not issubclass(enum_type, Enum):
+            raise ConfigurationError("{} is not a subclass of Enum!".format(enum_type))
+        self._enum_type = enum_type
+
+    def to_db_value(self, value: Enum, instance) -> str:
+        return value.name
+
+    def to_python_value(self, value: str) -> Enum:
+        try:
+            return self._enum_type(value)
+        except Exception:
+            raise ValueError(
+                "Database value {} does not exist on Enum {}.".format(
+                    value, self._enum_type
+                )
+            )
 
 
 class Player(Model):
-    id = fields.IntField(primary_key=True)
-    name = fields.CharField(max_length=255, unique=True)
+    id = IntField(primary_key=True)
+    name = CharField(max_length=CHAR_FIELD_LEN_NAMES, unique=True)
 
 
 class Game(Model):
-    id = fields.IntField(primary_key=True)
-    name = fields.CharField(max_length=255, unique=True)
+    id = IntField(primary_key=True)
+    name = CharField(max_length=CHAR_FIELD_LEN_NAMES, unique=True)
+    ranking_model = EnumField(enum_type=RankingModels, max_length=CHAR_FIELD_LEN_NAMES)
 
 
 class Match(Model):
-    id = fields.IntField(primary_key=True)
+    id = IntField(primary_key=True)
